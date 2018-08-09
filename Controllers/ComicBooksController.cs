1f3042cd638f8f23cb052bb91a695fd1854dfe81
rodrigosaizg <saizgonzalezrodrigo@gmail.com>
2018-08-09 15:55:04 +0200

    push

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBookGallery  : Controller
+     class ComicBookGallery  : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from de comic book controller";
+        }
     }
 }
