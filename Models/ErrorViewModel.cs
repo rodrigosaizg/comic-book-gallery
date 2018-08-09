3e3b48050ce2c9c7309ea76cc7a3a7e862bb601b
rodrigosaizg <saizgonzalezrodrigo@gmail.com>
2018-08-09 15:31:19 +0200

    push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace comic_book_gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
